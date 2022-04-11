b9f8df68402ef8fe70561d556a29aafa2c478c18
SdangondLT <sdangond@lean-tech.io>
2022-04-10 20:15:07 -0500

    added basic exercise about the routing with Angular

---
+++ src/app/pages/users/users-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserEditComponent } from './user-edit/user-edit.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UserListComponent } from './user-list/user-list.component';
+
+const routes: Routes = [
+  {path: 'UserEdit', component: UserEditComponent},
+  {path: 'UserCreate', component: UserCreateComponent},
+  {path: 'UserList', component: UserListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UsersRoutingModule { }
